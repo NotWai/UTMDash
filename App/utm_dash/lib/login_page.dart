989ca748980628c139101213aa799cb2e232551a
NotWai <88481451+NotWai@users.noreply.github.com>
2023-11-11 12:17:06 +0800

    testcommit

--- App/utm_dash/lib/login_page.dart
+++ App/utm_dash/lib/login_page.dart
@@ -88,7 +88,7 @@ class _LoginPageState extends State<LoginPage> {
           style: TextStyle(
               color: myColor, fontSize: 32, fontWeight: FontWeight.w500),
         ),
-        _buildGreyText("Please login with your information"),
+        _buildGreyText("Please login with your information?"),
         const SizedBox(height: 60),
         _buildGreyText("Email address"),
         _buildInputField(emailController),
