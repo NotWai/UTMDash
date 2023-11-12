e9dbdf47c40070b213d59c1e2064d10fd3a95538
LAPTOP-V235MKIU\ASUS <sarahrasli1705@gmail.com>
2023-11-12 20:19:02 +0800

    4 new file 12/11

---
+++ App/utm_dash/lib/CustLoginPage.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+//import 'package:test2/hub_page.dart'; // Adjust the import based on the actual file name
+
+
+class CustLoginPage extends StatefulWidget {
+  const CustLoginPage({super.key});
+
+  @override
+  State<CustLoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<CustLoginPage> {
+  late Color myColor;
+  late Size mediaSize;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool rememberUser = false;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    myColor = Theme.of(context).primaryColor;
+    mediaSize = MediaQuery.of(context).size;
+    return Container(
+      decoration: BoxDecoration(
+        color: myColor,
+        image: DecorationImage(
+          image: const AssetImage("assets/images/bg.png"),
+          fit: BoxFit.cover,
+          colorFilter:
+              ColorFilter.mode(myColor.withOpacity(0.2), BlendMode.dstATop),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0, // Remove elevation
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context); // Navigate back to the previous screen
+            },
+          ),
+        ),
+        body: Stack(
+          children: [
+            Positioned(top: 80, child: _buildTop()),
+            Positioned(bottom: 0, child: _buildBottom()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTop() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Transform.translate(
+          offset: Offset(0, -70),
+          child: Image.asset(
+          "assets/images/UTMDASH_LOGO.png", // Update with the actual path to your image
+          height: 400, // Set the desired height
+          width: 400,  // Set the desired width
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottom() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: _buildForm(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Welcome",
+          style: TextStyle(
+              color: myColor, fontSize: 32, fontWeight: FontWeight.w500),
+        ),
+        _buildGreyText("Please login with your information"),
+        const SizedBox(height: 60),
+        _buildGreyText("Email address"),
+        _buildInputField(emailController),
+        const SizedBox(height: 40),
+        _buildGreyText("Password"),
+        _buildInputField(passwordController, isPassword: true),
+        const SizedBox(height: 20),
+        _buildRememberForgot(),
+        const SizedBox(height: 20),
+        _buildLoginButton(),
+        const SizedBox(height: 20),
+        //_buildSignUpLink(),
+      ],
+    );
+  }
+
+  Widget _buildGreyText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.grey),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller,
+      {isPassword = false}) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        suffixIcon: isPassword ? Icon(Icons.remove_red_eye) : Icon(Icons.done),
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  Widget _buildRememberForgot() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: rememberUser,
+              onChanged: (value) {
+                setState(() {
+                  rememberUser = value!;
+                });
+              },
+            ),
+            _buildGreyText("Remember me"),
+          ],
+        ),
+        TextButton(
+          onPressed: () {},
+          child: _buildGreyText("I forgot my password"),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return ElevatedButton(
+      onPressed: () {
+        debugPrint("Email : ${emailController.text}");
+        debugPrint("Password : ${passwordController.text}");
+      },
+      style: ElevatedButton.styleFrom(
+        primary: Color(0xFFBE1C2D),
+        shape: const StadiumBorder(),
+        elevation: 20,
+        shadowColor: myColor,
+        minimumSize: const Size.fromHeight(60),
+      ),
+      child: const Text("LOGIN"),
+    );
+  }
+
+}
---
+++ App/utm_dash/lib/HomePage.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:test2/CustLoginPage.dart';
+import 'package:test2/AdminLoginPage.dart';
+import 'package:test2/HubLoginPage.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UTMDASH'),
+        backgroundColor: Color(0xFFBE1C2D),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFFBE1C2D),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text ('Login',
+    style: TextStyle(
+      color: Colors.black,
+      fontSize: 24,
+      // Add your desired text style properties here
+      fontWeight: FontWeight.bold,
+      // Other properties...
+    ),
+  ),
+              onTap: () {
+                _showLoginMenu(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xFFBE1C2D),
+          image: DecorationImage(
+            image: AssetImage("assets/images/your_image.png"), // Replace with your actual image path
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Color(0xFFBE1C2D).withOpacity(0.2),
+              BlendMode.dstATop,
+            ),
+          ),
+        ),
+child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/UTMDASH_LOGO.png", // Replace with your actual welcome image path
+                height: 600, // Set the desired height
+                width: 600,  // Set the desired width
+              ),
+              SizedBox(height: 20),
+        Transform.translate(
+          offset: Offset(0, -100),
+          child: Text(
+            'Welcome to Our App!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+                ),
+              ),
+        ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showLoginMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text('Customer'),
+                onTap: () {
+                  Navigator.pop(context); // Close the modal
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CustLoginPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Admin'),
+                onTap: () {
+                  Navigator.pop(context); // Close the modal
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AdminLoginPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Hub'),
+                onTap: () {
+                  Navigator.pop(context); // Close the modal
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HubLoginPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
