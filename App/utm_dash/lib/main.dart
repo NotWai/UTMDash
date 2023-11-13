44d4068b60dc15da6215752a3ff0fe5b3a7e3422
NotWai <88481451+NotWai@users.noreply.github.com>
2023-11-13 12:22:12 +0800

    Update main.dart

--- App/utm_dash/lib/main.dart
+++ App/utm_dash/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: MyApp(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
