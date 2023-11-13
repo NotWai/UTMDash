53809bacd3fb68c19d6a07d8c473e79158b20fe4
hsyad <78629701+hsyad@users.noreply.github.com>
2023-11-13 12:53:35 +0800

    add files

---
+++ App/utm_dash/lib/signout.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:utm_dash/reusable_widgets/reusable_widgets.dart';
+import 'package:utm_dash/screen/signup.dart';
+
+class SignOut extends StatefulWidget {
+  const SignOut({super.key});
+
+  @override
+  State<SignOut> createState() => SignOutState();
+}
+
+class SignOutState extends State<SignOut> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Color(0xFFBE1C2D),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: [
+                Image.asset("assets/images/successful.png",
+                    height: 200, width: 200),
+                const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "You are signed out of your account.",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                        textAlign:
+                            TextAlign.center, // Set text alignment to center
+                      ),
+                    ]),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+                signUpSignInLogoutButton(context, false, () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignUp()));
+                })
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ App/utm_dash/lib/signup.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:utm_dash/reusable_widgets/reusable_widgets.dart';
+import 'package:utm_dash/screen/signout.dart';
+//import 'package:utm_dash/screen/signout.dart';
+//import 'package:utm_dash/screen/testSignOut.dart';
+//import 'package:utm_dash/utils/color_utils.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final TextEditingController _fullNameTextController = TextEditingController();
+  final TextEditingController _phoneNumTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+  final TextEditingController _passwordTextController = TextEditingController();
+  final TextEditingController _confirmPassTextController =
+      TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Color(0xFFBE1C2D),
+          /*gradient: LinearGradient(colors: [
+          hexStringToColor("FEFDFD"), //white
+          hexStringToColor("FFC7D4"), //softpink
+          hexStringToColor("C42031"), //red
+          //hexStringToColor("0E0606"), //black
+        ],*/
+          //begin: Alignment.topCenter, end: Alignment.bottomCenter)
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/UTMDASH_LOGO.png"),
+                const Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(children: [
+                        Text("Welcome!",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24)),
+                      ]),
+                      Row(children: [
+                        Text("New here? Sign Up",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 16)),
+                      ]),
+                    ]),
+                const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+                const SizedBox(
+                  height: 12,
+                ),
+                reusableTextField("Enter Full Name", Icons.person_outline,
+                    false, _fullNameTextController),
+                const SizedBox(
+                  height: 12,
+                ),
+                reusableTextField("Enter Phone Number", Icons.phone_outlined,
+                    false, _phoneNumTextController),
+                const SizedBox(
+                  height: 12,
+                ),
+                reusableTextField("Enter Email Address", Icons.email_outlined,
+                    false, _emailTextController),
+                const SizedBox(
+                  height: 12,
+                ),
+                reusableTextField("Enter Password", Icons.lock_outline, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 12,
+                ),
+                reusableTextField("Confirm Password", Icons.lock_outline, true,
+                    _confirmPassTextController),
+                const SizedBox(
+                  height: 12,
+                ),
+                signUpSignInLogoutButton(context, true, () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const SignOut()));
+                })
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
