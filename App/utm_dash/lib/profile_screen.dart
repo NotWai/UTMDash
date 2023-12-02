e1e1961f6e38c7531832c5359b9f468ab64dbb9b
LAPTOP-V235MKIU\ASUS <sarahrasli1705@gmail.com>
2023-12-03 05:24:18 +0800

    edit profile cust

---
+++ App/utm_dash/lib/profile_screen.dart
@@ -0,0 +1,390 @@
+import 'package:flutter/material.dart';
+import 'package:utm_dash/screen/edit_profile.dart';
+import 'package:utm_dash/screen/signout.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Align(
+          alignment: const AlignmentDirectional(-1.00, -1.02),
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+            child: Container(
+              //width: 403,
+              height: 98,
+              decoration: const BoxDecoration(
+                color: Color(0xFFCC2C2C),
+              ),
+              child: const Align(
+                alignment: AlignmentDirectional(0.00, 0.00),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 16, 0, 0),
+                  child: Text('Profile',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24)),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: const AlignmentDirectional(0.00, 0.00),
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: const AlignmentDirectional(0.00, 0.00),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.network(
+                      "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Align(
+          alignment: AlignmentDirectional(0.00, 0.00),
+          child: Text('Zuhair Isyraq',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              )),
+        ),
+        const Align(
+          alignment: AlignmentDirectional(0.00, 0.00),
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 16),
+            child: Text(
+              'zuhairisyraq@gmail.com',
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(24, 4, 0, 0),
+          child: Text(
+            'Your Account',
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+          child: InkWell(
+            splashColor: Colors.transparent,
+            focusColor: Colors.transparent,
+            hoverColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const EditProfile()));
+            },
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 3,
+                    color: Color(0x33000000),
+                    offset: Offset(0, 1),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(8),
+                shape: BoxShape.rectangle,
+              ),
+              child: Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
+                child: InkWell(
+                  splashColor: Colors.transparent,
+                  focusColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onTap: () async {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const Icon(
+                        Icons.account_circle_outlined,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                        size: 24,
+                      ),
+                      const Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                        child: Text(
+                          'Edit Profile',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 16,
+                            color: Color.fromRGBO(90, 92, 96, 1),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Align(
+                          alignment: const AlignmentDirectional(0.90, 0.00),
+                          child: InkWell(
+                            splashColor: Colors.transparent,
+                            focusColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const EditProfile()));
+                            },
+                            child: const Icon(
+                              Icons.arrow_forward_ios,
+                              color: Color.fromRGBO(90, 92, 96, 1),
+                              size: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+          child: Container(
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 3,
+                  color: Color(0x33000000),
+                  offset: Offset(0, 1),
+                )
+              ],
+              borderRadius: BorderRadius.circular(8),
+              shape: BoxShape.rectangle,
+            ),
+            child: const Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(
+                    Icons.notifications_none,
+                    color: Color.fromRGBO(90, 92, 96, 1),
+                    size: 24,
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                    child: Text(
+                      'Notification Settings',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: AlignmentDirectional(0.90, 0.00),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                        size: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(24, 16, 0, 0),
+          child: Text(
+            'App Settings',
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 16,
+              color: Color.fromRGBO(90, 92, 96, 1),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+          child: Container(
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 3,
+                  color: Color(0x33000000),
+                  offset: Offset(0, 1),
+                )
+              ],
+              borderRadius: BorderRadius.circular(8),
+              shape: BoxShape.rectangle,
+            ),
+            child: const Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(
+                    Icons.help_outline_rounded,
+                    color: Color.fromRGBO(90, 92, 96, 1),
+                    size: 24,
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                    child: Text(
+                      'Support',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: AlignmentDirectional(0.90, 0.00),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                        size: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+          child: Container(
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 3,
+                  color: Color(0x33000000),
+                  offset: Offset(0, 1),
+                )
+              ],
+              borderRadius: BorderRadius.circular(8),
+              shape: BoxShape.rectangle,
+            ),
+            child: const Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(12, 12, 12, 12),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(
+                    Icons.privacy_tip_rounded,
+                    color: Color.fromRGBO(90, 92, 96, 1),
+                    size: 24,
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                    child: Text(
+                      'Terms of Service',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: AlignmentDirectional(0.90, 0.00),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color.fromRGBO(90, 92, 96, 1),
+                        size: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: const AlignmentDirectional(0.00, 0.00),
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+            child: ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const SignOut()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFBE1C2D),
+                  padding: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38),
+                  ),
+                ),
+                child: Container(
+                  width: 150,
+                  height: 44,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                )),
+          ),
+        ),
+      ],
+    )));
+  }
+}
