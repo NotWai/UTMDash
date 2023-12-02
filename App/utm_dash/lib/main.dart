5d60545b18e12f985f550879cf410aed84301387
Mohammed Hussein <saleh07@graduate.utm.my>
2023-12-03 00:55:34 +0800

    Database_connect

--- App/utm_dash/lib/main.dart
+++ App/utm_dash/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:utm_dash/CustLoginPage.dart';
 import 'package:utm_dash/HomePage.dart';
@@ -5,8 +7,19 @@ import 'package:utm_dash/request_box.dart';
 import 'package:utm_dash/request_details.dart';
 import 'package:utm_dash/signup.dart';
 import 'package:utm_dash/viewCustomerPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyA1dPSUPvWXJQiJldbYekcdljcLBfEIRQ4",
+      appId: "1:274283878114:android:dc7123b4b882a9c78c70bb",
+      messagingSenderId: "274283878114",
+      projectId: "utmdash-a9abf",
+    ),
+  );
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
