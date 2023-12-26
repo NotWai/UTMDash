318d5479ed6f6d81a11ff5f57aa9a477a172f505
NotWai <88481451+NotWai@users.noreply.github.com>
2023-12-26 21:28:05 +0800

    Update main.dart

--- App/utm_dash/lib/main.dart
+++ App/utm_dash/lib/main.dart
@@ -6,6 +6,13 @@ import 'package:utm_dash/screens/wrapper.dart';
 import 'package:utm_dash/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+//import 'package:utm_dash/HomePage.dart';
+//import 'package:utm_dash/HomePageUser.dart';
+//import 'package:utm_dash/IntroPage.dart';
+//import 'package:utm_dash/request_box.dart';
+//import 'package:utm_dash/request_details.dart';
+//import 'package:utm_dash/signup.dart';
+//import 'package:utm_dash/viewCustomerPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
